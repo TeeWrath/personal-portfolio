53bcc9e84691a46a0b5dc621ca3f2890e607dad2
TeeWrath <subroto.2003@gmail.com>
2023-08-19 01:29:08 +0530

    completed hero section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
+import { SocialsComponent } from './main/socials/socials.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    SocialsComponent
   ],
   imports: [
     BrowserModule,
