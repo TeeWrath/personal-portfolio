6566a2831f2575f8f3cbc45569db3d70218c9dc5
TeeWrath <subroto.2003@gmail.com>
2023-08-20 04:09:02 +0530

    added required components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SocialMainComponent } from './social-main/social-main.component';
+import { AboutComponent } from './about/about.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { MusicComponent } from './music/music.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
     NavbarComponent,
-    SocialMainComponent
+    SocialMainComponent,
+    AboutComponent,
+    ExperienceComponent,
+    ProjectsComponent,
+    MusicComponent
   ],
   imports: [
     BrowserModule,
