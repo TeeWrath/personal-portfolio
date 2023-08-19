fa601277b42419b1541a218ce97c0bfa7fcaa946
TeeWrath <subroto.2003@gmail.com>
2023-08-20 04:22:44 +0530

    added routing in navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { AboutComponent } from './about/about.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { MusicComponent } from './music/music.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '', component : MainComponent},
+  {path : 'about', component : AboutComponent},
+  {path : 'experience', component : ExperienceComponent},
+  {path : 'project', component : ProjectsComponent},
+  {path : 'music', component : MusicComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
